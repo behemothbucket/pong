a0e24c1ce9fae44f11c5aa70db078cb8de4f1354
behemothbucket <mahagon.here@gmail.com>
2023-10-24 23:31:53 +0300

    fix: refactor ball and score logic

--- ball.go
+++ ball.go
@@ -2,40 +2,65 @@ package main
 
 import (
 	"image/color"
+	"math/rand"
+	"time"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/vector"
 )
 
 type Ball struct {
-	x, y           float32
-	speedX, speedY float32
-	radius         float32
-	color          *color.Gray16
+	x, y       float32
+	directionX float32
+	directionY float32
+	radius     float32
+	color      *color.Gray16
 }
 
 func (b *Ball) HandleCollisions(g *Game) {
-	if b.x < 0 || b.x > screenWidth-b.radius {
-		b.speedX = -b.speedX
-
+	if b.x < -b.radius || b.x > screenWidth+b.radius {
 		playerIndex := 0
+
 		if b.x < 0 {
 			playerIndex = 1
 		}
 
 		g.score[playerIndex]++
+		g.scored = true
+		g.timeout = ebiten.TPS()
 	}
 
 	if b.y < 0 || b.y > screenHeight-b.radius {
-		b.speedY = -b.speedY
+		b.directionY = -b.directionY
 	}
 }
 
 func (b *Ball) Update(g *Game) {
-	b.x += b.speedX
-	b.y += b.speedY
+	if g.timeout > 0 {
+		g.timeout--
+		return
+	}
+
+	if g.scored {
+		b.RandomDirection()
+		g.scored = false
+	}
+
+	b.x += b.directionX
+	b.y += b.directionY
 
 	b.HandleCollisions(g)
+}
+
+func (b *Ball) RandomDirection() {
+	source := rand.NewSource(time.Now().UnixNano())
+	random := rand.New(source)
+
+	b.x = screenWidth / 2
+	b.y = float32(random.Intn(480) + 1)
+
+	b.directionX = float32(4 - 8*random.Intn(2))
+	b.directionY = float32(4 - 8*random.Intn(2))
 }
 
 func (b *Ball) Draw(screen *ebiten.Image) {
