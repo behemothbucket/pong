c0d0fa0b9c3aecc48d0c3806374bd8d7e55c709a
behemothbucket <mahagon.here@gmail.com>
2023-10-24 10:15:50 +0300

    refactor: all structs, methods for structs

--- main.go
+++ main.go
@@ -15,23 +15,129 @@ const (
 	screenHeight = 600
 )
 
-type Game struct{}
-
-var (
+type Game struct {
 	rocket *Rocket
 	ball   *Ball
-)
+	field  *Field
+}
+
+type Field struct {
+	fieldLine *FieldLine
+}
+
+type FieldLine struct {
+	x       float32
+	y       float32
+	width   float32
+	spacing float32
+	color   *color.Gray16
+}
 
 type Rocket struct {
 	x, y   float32
 	width  float32
 	height float32
+	color  *color.Gray16
 }
 
 type Ball struct {
 	x, y           float32
 	speedX, speedY float32
 	size           float32
+	color          *color.Gray16
+}
+
+func main() {
+	ebiten.SetWindowSize(screenWidth, screenHeight)
+	ebiten.SetWindowTitle("        Pong")
+
+	game := newGame()
+
+	if err := ebiten.RunGame(game); err != nil {
+		log.Fatalf("Ошибка при запуске игры: %v", err)
+	}
+}
+
+func newGame() *Game {
+	field := &Field{
+		fieldLine: &FieldLine{
+			x:       screenWidth / 2,
+			y:       0,
+			width:   2,
+			spacing: 10,
+			color:   &color.White,
+		},
+	}
+
+	rocket := &Rocket{
+		x:      10,
+		y:      screenHeight / 2,
+		width:  10,
+		height: 50,
+		color:  &color.White,
+	}
+
+	ball := &Ball{
+		x:      screenWidth / 2,
+		y:      screenHeight / 2,
+		size:   20,
+		speedX: 6,
+		speedY: 6,
+		color:  &color.White,
+	}
+
+	return &Game{
+		field:  field,
+		rocket: rocket,
+		ball:   ball,
+	}
+}
+
+func (fl *FieldLine) Draw(screen *ebiten.Image) {
+	for y := fl.y; y < screenHeight; y += fl.spacing {
+		vector.DrawFilledRect(screen, fl.x, y, fl.width, fl.width+fl.spacing/2, fl.color, false)
+	}
+}
+
+func (f *Field) Draw(screen *ebiten.Image) {
+	f.fieldLine.Draw(screen)
+}
+
+func (r *Rocket) Update() {
+	_, mouseY := ebiten.CursorPosition()
+	r.y = float32(mouseY) - r.height/2
+}
+
+func (r *Rocket) Draw(screen *ebiten.Image) {
+	ry := r.y
+
+	if ry < 0 {
+		ry = 0
+	} else if ry > screenHeight-r.height {
+		ry = screenHeight - r.height
+	}
+
+	vector.DrawFilledRect(screen, r.x, ry, r.width, r.height, r.color, false)
+}
+
+func (b *Ball) HandleCollisions() {
+	if b.x < 0 || b.x > screenWidth-b.size {
+		b.speedX = -b.speedX
+	}
+	if b.y < 0 || b.y > screenHeight-b.size {
+		b.speedY = -b.speedY
+	}
+}
+
+func (b *Ball) Update() {
+	b.x += b.speedX
+	b.y += b.speedY
+
+	b.HandleCollisions()
+}
+
+func (b *Ball) Draw(screen *ebiten.Image) {
+	vector.DrawFilledRect(screen, b.x, b.y, b.size, b.size, b.color, false)
 }
 
 func (g *Game) Update() error {
@@ -39,75 +145,18 @@ func (g *Game) Update() error {
 		return errors.New("quit")
 	}
 
-	_, mouseY := ebiten.CursorPosition()
-
-	rocket.y = float32(mouseY) - rocket.height/2
-
-	ball.x += ball.speedX
-	ball.y += ball.speedY
-
-	if ball.x < 0 || ball.x > screenWidth-ball.size {
-		ball.speedX = -ball.speedX
-	}
-	if ball.y < 0 || ball.y > screenHeight-ball.size {
-		ball.speedY = -ball.speedY
-	}
+	g.rocket.Update()
+	g.ball.Update()
 
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	// FIX StrokeLine -> DrawFilledRect
-	var lineX float32 = screenWidth / 2
-	var lineY1 float32 = 0
-	var lineY2 float32 = screenHeight
-	var lineSpacing float32 = 10
-
-	lineColor := color.White
-	var lineWidth float32 = 2
-
-	for y := lineY1; y < lineY2; y += lineSpacing {
-		vector.StrokeLine(screen, lineX, y, lineX, y+lineSpacing/2, lineWidth, lineColor, false)
-	}
-
-	vector.DrawFilledRect(screen, ball.x, ball.y, ball.size, ball.size, color.White, false)
-
-	if rocket.y >= screenHeight-rocket.height {
-		vector.DrawFilledRect(screen, rocket.x, screenHeight-rocket.height, rocket.width, rocket.height, lineColor, false)
-	}
-
-	if rocket.y <= 0 {
-		vector.DrawFilledRect(screen, rocket.x, 0, rocket.width, rocket.height, lineColor, false)
-	} else {
-		vector.DrawFilledRect(screen, rocket.x, rocket.y, rocket.width, rocket.height, lineColor, false)
-	}
+	g.field.Draw(screen)
+	g.rocket.Draw(screen)
+	g.ball.Draw(screen)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 	return screenWidth, screenHeight
-}
-
-func main() {
-	ebiten.SetWindowSize(screenWidth, screenHeight)
-	ebiten.SetWindowTitle("        Pong")
-
-	rocket = &Rocket{
-		x:      10,
-		y:      screenHeight / 2,
-		width:  10,
-		height: 50,
-	}
-
-	ball = &Ball{
-		x:      screenWidth / 2,
-		y:      screenHeight / 2,
-		size:   20,
-		speedX: 6,
-		speedY: 6,
-	}
-
-	game := &Game{}
-	if err := ebiten.RunGame(game); err != nil {
-		log.Fatal(err)
-	}
 }
